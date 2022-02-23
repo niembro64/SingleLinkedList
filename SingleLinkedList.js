450aa6ac78c09bead35c71aa061c01a1423b5bb9
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-23 10:02:31 -0600

    Init

--- SingleLinkedList.js
+++ SingleLinkedList.js
@@ -1,6 +1,3 @@
-// Singly Linked List
-
-
 class Node{
     constructor(value){
         this.value = value
@@ -8,67 +5,47 @@ class Node{
     }
 }
 
-
-
-class SLList{
-    constructor(){
+// a queue operates on the principal of "First In, First Out" like waiting in line for something
+class SLQueue{
+    constructor() {
         this.head = null
+        this.tail = null
     }
 
-    addToFront(value) {
-        // step #1 Make a new node
-        var newNode = new Node(value);
+    // add a node with the given value to the queue
+    enqueue(value) {
         
-        // Check to see if there is a head
-        if(this.head == null) {
-            this.head = newNode;
+    
+    }
 
-            return this;
+    // remove and return the front value from the queue
+    dequeue() {
+
+
+    }
+
+    //return true/false based on whether you find the given value in a queue
+    contains(value) {
+    }
+
+    // remove the minimum value in the queue (remember your edgecases and pointers!)
+    removeMin() {
+    }
+
+    displayQueue(){
+        if (!this.head){
+            console.log("This queue is empty.");
         }
-
-        // if there is a head
-        newNode.next = this.head;
-        this.head = newNode;
-
-        return this;
-
-
-    }
-
-    addToBack(value) {
-
-    }
-
-    removeFromFront() {
-
-    }
-
-    removeFromBack() {
-
-   }
-
-   // print the singly linked list
-    printValues() {
-        // step #0 [EDGE CASE]) handle a case where there is nothing in the list
-        if(this.head == null){
-            console.log("There's nothing in the list! Dummy!")
-            // return 'this' to end function and allow chaining of methods
-            return this
+        else {
+            var runner = this.head;
+            var str = "";
+            while(runner){
+                str += runner.value + " -> ";
+                runner = runner.next;
+            }
+            str += "null";
+            console.log(str);
         }
-        //step #1) establish a runner to traverse through the list
-        var runner = this.head;
-
-        // NOTE: we can move runner all the way into null because our loop will exit as soon as runner hits null, avoiding any errors with printing
-        while(runner != null) {
-            // step #2) print the values at each iteration before moving the runner!
-            console.log(`The current value is: ${runner.value}`)
-            runner = runner.next
-        }
-        console.log("We have hit the end of the list!")
-        // return 'this' to end function and allow chaining of methods
-        return this
     }
 
-const sll = new SLList();
-sll.addToFront(-3)
-sll.printValues()
+}
